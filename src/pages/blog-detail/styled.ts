ef9eb6264108f13f03c583226c6830904f9c0661
sillyy <965768042@qq.com>
2020-07-09 19:09:03 +0800

    登录模块

---
+++ src/pages/blog-detail/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Warp = styled.div`
+  width: 100%;
+  background-color: #fff;
+  border-radius: 2px;
+  box-sizing: border-box;
+`
+
+export const Article = styled.article`
+  margin: 25px 0;
+  padding: 20px;
+`
