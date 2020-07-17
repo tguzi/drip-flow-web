f05a533b9d2457fdac58f004c068614d6e47c779
qingfu.wu <642935965@qq.com>
2020-07-17 22:42:36 +0800

    自动发布测试

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 
 import Layout from 'layout'
-import Content from './content'
+// import Content from './content'
 import { AddBlogBtn } from './styled'
-
-console.log('github actions')
 
 const Homepage = () => {
   const history = useHistory()
-  console.log('我是小小到github actions测试')
   return (
     <Layout layout="content-center">
-      <Content />
+      {/* <Content /> */}
       <AddBlogBtn
         onClick={() => {
           history.push({
