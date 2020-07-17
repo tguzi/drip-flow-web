4fd0aa82d377843d1595c133566eba50d0587161
qingfu.wu <642935965@qq.com>
2020-07-17 22:46:56 +0800

    自动发布v1.0

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 
 import Layout from 'layout'
-// import Content from './content'
+import Content from './content'
 import { AddBlogBtn } from './styled'
 
 const Homepage = () => {
   const history = useHistory()
   return (
     <Layout layout="content-center">
-      {/* <Content /> */}
+      <Content />
       <AddBlogBtn
         onClick={() => {
           history.push({
